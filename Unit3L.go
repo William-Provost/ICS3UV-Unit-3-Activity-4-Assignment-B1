1d69a7f6f3e0194e63f4c8065fdd0e4b1504fb17
default <william.provost@stu.ocsb.ca>
2025-11-17 23:35:56 +0000

    fixed linter error

--- Unit3L.go
+++ Unit3L.go
@@ -14,9 +14,9 @@ import (
 
 func main() {
 	// variable declaration
-	var colour string = ""
+	var colour string
 
-	var reader = bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
 	// input colour
 	fmt.Print("Please choose a sweater colour from the available choices: Blue, Black, Red, White.\n")
@@ -24,14 +24,15 @@ func main() {
 	colour = strings.TrimSpace(colour)
 
 	// check colour choice
-	if strings.ToLower(colour) == "black" || strings.ToLower(colour) == "white" {
+	switch strings.ToLower(colour) {
+	case "black", "white":
 		fmt.Println("You have enough sweaters in this colour.")
-	} else if strings.ToLower(colour) == "red" {
+	case "red":
 		fmt.Println("This colour will look good on you.")
 		fmt.Println("How about a pair of jeans to go with the sweater?")
-	} else if strings.ToLower(colour) == "blue" {
+	case "blue":
 		fmt.Println("This colour doesn’t go well with your eyes.")
-	} else {
+	default:
 		fmt.Println("Your colour choice is invalid.")
 	}
 
